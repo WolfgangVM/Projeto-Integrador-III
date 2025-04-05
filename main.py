4304ccd3c3e1c62c31452f0d03b55b6143720e3c
Wolfgang Vinícius Manzato <108307517+WolfgangVM@users.noreply.github.com>
2025-04-05 17:13:36 -0300

    mudanças em main, views e models. As mudanças foramm a adição do SQLAlchemy, Bcrypt e loginManager no main, no views foi adicionado registro, login, verificação para login e rota de logout

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 #usado para banco de dados, se não usar deixar comentado
 #flask sqlalchemy
@@ -6,8 +9,17 @@ from flask import Flask
 #usado para formularios, se não usar deixar comentado
 #flask wtf forms
 
-
 app = Flask(__name__)
+
+# Configurações
+app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# Inicializações
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from views import *
 
--- views.py
+++ views.py
@@ -1,8 +1,44 @@
-from main import app
-from flask import render_template
+from main import app, db, bcrypt
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required, current_user
+from models import User  # Certifique-se de criar o modelo User no arquivo models.py
 
 # Rotas
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        user = User(username=username, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Conta criada com sucesso!", "success")
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash("Login realizado com sucesso!", "success")
+            return redirect(url_for("homepage"))
+        else:
+            flash("Falha no login. Verifique seu usuário e senha.", "danger")
+    return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Você saiu da sua conta.", "info")
+    return redirect(url_for("login"))
